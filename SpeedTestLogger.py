60de06c23cf480e01ac04050fed194c51a5185f1
AMasetti <32473909+AMasetti@users.noreply.github.com>
2020-03-16 15:43:03 -0300

    Update SpeedTestLogger.py

--- SpeedTestLogger.py
+++ SpeedTestLogger.py
@@ -36,53 +36,7 @@ with open('LogSpeedTest.csv','a') as file:
     wr = csv.writer(file, quoting=csv.QUOTE_ALL)
     wr.writerow([Date, Hour, DownloadSpeed, UploadSpeed, Ping, IP, ISP, City, Country])
 
+# with open('FTP/files/LogSpeedTest.csv','a') as file:
+#     wr = csv.writer(file, quoting=csv.QUOTE_ALL)
+#     wr.writerow([Date, Hour, DownloadSpeed, UploadSpeed, Ping, IP, ISP, City, Country])
 
-# To run every hour
-# 0 0 * * * python /home/pi/Speedtest.py
-# 0 1 * * * python /home/pi/Speedtest.py
-# 0 2 * * * python /home/pi/Speedtest.py
-# 0 3 * * * python /home/pi/Speedtest.py
-# 0 4 * * * python /home/pi/Speedtest.py
-# 0 5 * * * python /home/pi/Speedtest.py
-# 0 6 * * * python /home/pi/Speedtest.py
-# 0 7 * * * python /home/pi/Speedtest.py
-# 0 8 * * * python /home/pi/Speedtest.py
-# 0 9 * * * python /home/pi/Speedtest.py
-# 0 10 * * * python /home/pi/Speedtest.py
-# 0 12 * * * python /home/pi/Speedtest.py
-# 0 13 * * * python /home/pi/Speedtest.py
-# 0 14 * * * python /home/pi/Speedtest.py
-# 0 15 * * * python /home/pi/Speedtest.py
-# 0 16 * * * python /home/pi/Speedtest.py
-# 0 17 * * * python /home/pi/Speedtest.py
-# 0 18 * * * python /home/pi/Speedtest.py
-# 0 19 * * * python /home/pi/Speedtest.py
-# 0 20 * * * python /home/pi/Speedtest.py
-# 0 21 * * * python /home/pi/Speedtest.py
-# 0 22 * * * python /home/pi/Speedtest.py
-# 0 23 * * * python /home/pi/Speedtest.py
-
-# To run every half hour
-# 30 0 * * * python /home/pi/Speedtest.py
-# 30 1 * * * python /home/pi/Speedtest.py
-# 30 2 * * * python /home/pi/Speedtest.py
-# 30 3 * * * python /home/pi/Speedtest.py
-# 30 4 * * * python /home/pi/Speedtest.py
-# 30 5 * * * python /home/pi/Speedtest.py
-# 30 6 * * * python /home/pi/Speedtest.py
-# 30 7 * * * python /home/pi/Speedtest.py
-# 30 8 * * * python /home/pi/Speedtest.py
-# 30 9 * * * python /home/pi/Speedtest.py
-# 30 10 * * * python /home/pi/Speedtest.py
-# 30 12 * * * python /home/pi/Speedtest.py
-# 30 13 * * * python /home/pi/Speedtest.py
-# 30 14 * * * python /home/pi/Speedtest.py
-# 30 15 * * * python /home/pi/Speedtest.py
-# 30 16 * * * python /home/pi/Speedtest.py
-# 30 17 * * * python /home/pi/Speedtest.py
-# 30 18 * * * python /home/pi/Speedtest.py
-# 30 19 * * * python /home/pi/Speedtest.py
-# 30 20 * * * python /home/pi/Speedtest.py
-# 30 21 * * * python /home/pi/Speedtest.py
-# 30 22 * * * python /home/pi/Speedtest.py
-# 30 23 * * * python /home/pi/Speedtest.py
